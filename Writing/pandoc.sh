073bb70c631da652d1bd39fc67863ad58ca14998
TGuillerme <guillert@tcd.ie>
2020-05-05 11:29:06 +0100

    compiler with color tags

--- Writing/pandoc.sh
+++ Writing/pandoc.sh
@@ -1,1 +1,5 @@
-pandoc -o disparity_review.docx -i disparity_review.tex --bibliography References.bib --filter pandoc-citeproc --csl biologyletters.csl
+sed 's/textcolor{blue}{/textcolor{blue}{COLORTAGIN[/g' disparity_review.tex > tmp
+sed 's/}/]COLORTAGOUT}/g' tmp > compile.tex
+pandoc -o disparity_review.docx -i compile.tex --bibliography References.bib --filter pandoc-citeproc --csl biologyletters.csl
+rm compile.tex
+rm tmp
